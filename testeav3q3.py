59c9956f6239bf63a6b1c648787a82bee1dda56d
Wellington Antonio do Nascimento <69446679+Wnascimento33@users.noreply.github.com>
2021-12-06 19:43:31 -0300

    funções anônimas
    para praticar

---
+++ testeav3q3.py
@@ -0,0 +1,7 @@
+minha_lista = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+lista_par = list(filter(lambda x: x % 2 == 0,minha_lista))
+print(list(lista_par))
+
+
+#Aluno: Wellington Antonio do Nascimento
+#Mat:202104150725
